b8d8d932baf225cc2924c1cead0ed6bb1712e8d1
porovoz <falkon1@list.ru>
2023-06-09 14:37:32 +0300

    initial commit

---
+++ src/main/java/com/bestapp/practicebot/PracticeBotApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.practicebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestapp/practicebot/PracticeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestapp.practicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
